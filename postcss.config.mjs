919543f4ad49b1b5ba567b65e9eb59ec5bc6e9c9
Shubham Gupta <shubhamguptacoder@gmail.com>
2025-08-31 09:47:19 +0530

    project setup complete

---
+++ postcss.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {
+      content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+      ],
+    },
+  },
+};
